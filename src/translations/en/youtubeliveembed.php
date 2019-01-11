128947c6ece77e3ab7695de56df4a84fab3a9983
Andrew Welch <andrew@nystudio107.com>
2019-01-11 18:35:07 -0500

    Fix missing translations
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

---
+++ src/translations/en/youtubeliveembed.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * YouTube Live Embed plugin for Craft CMS 3.x
+ *
+ * This plugin allows you to embed a YouTube live stream and/or live chat on your webpage
+ *
+ * @link      https://nystudio107.com
+ * @copyright Copyright (c) 2019 nystudio107
+ */
+
+/**
+ * @author    nystudio107
+ * @package   YoutubeLiveEmbed
+ * @since     1.0.0
+ */
+return [
+    '{name} plugin loaded' => '{name} plugin loaded',
+    'YouTube Channel ID' => 'YouTube Channel ID',
+    'Error rendering template string -> {error}' => 'Error rendering template string -> {error}',
+    'Error rendering `{template}` -> {error}' => 'Error rendering `{template}` -> {error}',
+    'Enter your YouTube channel ID here. If you do not know it, [here is how to find it](https://support.google.com/youtube/answer/3250431?hl=en).' => 'Enter your YouTube channel ID here. If you do not know it, [here is how to find it](https://support.google.com/youtube/answer/3250431?hl=en).'
+];
