3a8306b6be6908590ebe61575c878333cc672381
Andrew Welch <andrew@nystudio107.com>
2019-02-01 17:57:18 -0500

    Added a controller endpoint so the `is-live` and `live-viewers` can be pinged dynamically via JavaScript
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

---
+++ src/controllers/InfoController.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * YouTube Live Embed plugin for Craft CMS 3.x
+ *
+ * This plugin allows you to embed a YouTube live stream and/or live chat on your webpage
+ *
+ * @link      https://nystudio107.com
+ * @copyright Copyright (c) 2019 nystudio107
+ */
+
+namespace nystudio107\youtubeliveembed\controllers;
+
+use nystudio107\youtubeliveembed\YoutubeLiveEmbed;
+
+use Craft;
+use craft\web\Controller;
+
+/**
+ * @author    nystudio107
+ * @package   YoutubeLiveEmbed
+ * @since     1.0.0
+ */
+class InfoController extends Controller
+{
+    // Protected Properties
+    // =========================================================================
+
+    protected $allowAnonymous = [
+        'is-live',
+        'live-viewers',
+    ];
+
+    // Public Methods
+    // =========================================================================
+
+
+    /**
+     * Returns whether the stream is currently live
+     *
+     * @return bool
+     */
+    public function actionIsLive(): bool
+    {
+        return YoutubeLiveEmbed::$plugin->embed->isLive();
+    }
+
+    /**
+     * Returns the number of people currently viewing the live stream
+     *
+     * @return int
+     */
+    public function actionLiveViewers(): int
+    {
+        return YoutubeLiveEmbed::$plugin->embed->liveViewers();
+    }
+}
